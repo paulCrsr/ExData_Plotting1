cfb6b60ac8f41064726ff2f2a0def6cd608429b3
Paul Osborne <paul@vevista.com>
2017-03-18 12:32:03 +0000

    Completed plot 3.

---
+++ plot3.R
@@ -0,0 +1,26 @@
+source("ingestData.R")
+
+png("plot3.png",
+        width = 480,
+        height = 480)
+
+with(household, {
+        plot(householdDateTime(Date, Time), 
+             Sub_metering_1,
+             type = "l",
+             col = "black",
+             main = "",
+             ylab = "Energy sub metering",
+             xlab = ""             
+             )
+        points(householdDateTime(Date, Time), Sub_metering_2, type = "l", col = "red")
+        points(householdDateTime(Date, Time), Sub_metering_3, type = "l", col = "blue")
+})
+
+legend("topright", 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1,1),
+       col = c("black", "red", "blue")
+       )
+
+dev.off()
